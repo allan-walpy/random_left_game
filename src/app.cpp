c2a9eb7b1da05d2a5acd97c2e0fa8ff3e84c842d
allan_walpy <allanwalpy@gmail.com>
2022-04-05 23:50:16 +0300

    smth

---
+++ src/app.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const string app_name = "random_left_game";
+const string app_version = "0.1.0-zero";
+
+int main()
+{
+    cout << "app" << endl;
+    cout << "created by allan_walpy" << endl;
+
+    int a = 5 + 5, b = 2713;
+
+    cout << a - b;
+
+    return 0;
+}
