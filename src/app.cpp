01bcf8fd3ccb834f37bc2763b6e084ae87f0f450
allan_walpy <allanwalpy@gmail.com>
2022-04-06 00:26:32 +0300

    fooling around

--- src/app.cpp
+++ src/app.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 const string app_name = "random_left_game";
 const string app_version = "0.1.0-zero";
+const string app_author = "allan_walpy";
 
 int main()
 {
-    cout << "app" << endl;
-    cout << "created by allan_walpy" << endl;
+    int a = 5 + 5, b = 2713, c;
 
-    int a = 5 + 5, b = 2713;
+    cout << a << "-" << b << " = ";
+    cin >> c;
+    cout << ((c == a - b) ? "correct 👌" : "WRONG!") << endl;
 
-    cout << a - b;
+    string info = app_name + " v" + app_version + " created by " + app_author;
+    info = info.append(";");
+    cout << info << endl;
 
     return 0;
 }
