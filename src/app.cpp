ea788e3c0b527d8c81281f64b3d54baf671a2aa0
allan_walpy <allanwalpy@gmail.com>
2022-04-06 03:58:19 +0300

    fooling around

--- src/app.cpp
+++ src/app.cpp
@@ -8,30 +8,32 @@ const string app_name = "random_left_game";
 const string app_version = "0.1.0-zero";
 const string app_author = "allan_walpy";
 
+class operation_data
+{
+public:
+    int first;
+    int second;
+
+    int answer;
+
+    bool isCorrect()
+    {
+        return answer == first - second;
+    }
+};
+
 int main()
 {
-
     string info = app_name + " v" + app_version + " created by " + app_author + " >_> ☭ ";
     info = info.append(";");
     cout << info << endl;
-    cout << info.length() << " vs " << info.size() << endl;
 
-    cout << sqrt(2713);
-
-    for (int i = 0; i <= 2713; i++)
-    {
-        double sqrt_result = sqrt(i);
-        if (sqrt_result < 43 && sqrt_result > 41)
-        {
-            cout << " i = " << i << "; sqrt = " << sqrt_result << endl;
-        }
-    }
-
-    int a = 5 + 5, b = 2713, c;
-
-    cout << a << "-" << b << " = ";
-    cin >> c;
-    cout << ((c == a - b) ? "correct 👌" : "WRONG!") << endl;
+    operation_data data;
+    data.first = 13;
+    data.second = 27;
+    cout << data.first << "-" << data.second << " = ";
+    cin >> data.answer;
+    cout << (data.isCorrect() ? "correct 👌" : "WRONG!") << endl;
 
     return 0;
 }
