1a0c95a40b5275e26cda049e0bd8c6c587208adc
allan_walpy <allanwalpy@gmail.com>
2022-04-06 07:10:20 +0300

    add args

--- app.cpp
+++ app.cpp
@@ -9,7 +9,7 @@ const string app_name = "random_left_game";
 const string app_version = "0.1.1-zero";
 const string app_author = "allan_walpy";
 
-int main()
+int main(int argc, char* argv[])
 {
     string info = app_name + " v" + app_version + " created by " + app_author + " >_> ☭ ";
     info = info.append(";");
