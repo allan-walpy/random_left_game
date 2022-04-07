206978b6c5ca1a5526654b50b30ece72172a8b0d
allan_walpy <allanwalpy@gmail.com>
2022-04-08 01:00:28 +0300

    fix?

--- src/about.h
+++ src/about.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <string>
-#include "versionInfo.h"
+#include "versionInfo.cpp"
 
 const std::string app_name = "random_left_game";
 const VersionInfo app_version = VersionInfo(0, 1, 2, "a.zero");
 const std::string app_author = "allan_walpy";
-const std::string app_footer = app_name + " v" +  " created by " + app_author + ";";
-//app_version.toString() +
+const std::string app_footer = app_name + " v" + app_version.toString() + " created by " + app_author + ";";
--- src/versionInfo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-class VersionInfo
-{
-public:
-    VersionInfo(int, int, int, std::string = "");
-};
