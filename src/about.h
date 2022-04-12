e344fcc9ea5a17fa0d5c510932ecd6424c6e79eb
allan_walpy <allanwalpy@gmail.com>
2022-04-13 01:32:46 +0300

    dmp

--- src/about.h
+++ src/about.h
@@ -7,3 +7,8 @@ const std::string app_name = "random_left_game";
 const VersionInfo app_version = VersionInfo(0, 1, 2, "a.zero");
 const std::string app_author = "allan_walpy";
 const std::string app_footer = app_name + " v" + app_version.toString() + " created by " + app_author + ";";
+
+void print_footer()
+{
+    cout << endl << app_footer << endl;
+}
--- src/app.cpp
+++ src/app.cpp
@@ -3,16 +3,8 @@
 
 using namespace std;
 
-void print_footer();
-
 int main(int argc, char *argv[])
 {
     print_footer();
     return 0;
-}
-
-void print_footer()
-{
-    cout << endl
-         << app_footer << endl;
 }
