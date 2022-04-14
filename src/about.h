bfceb75023dfb9f4c52e759860bf31cd5e481bec
allan_walpy <allanwalpy@gmail.com>
2022-04-14 15:12:15 +0300

    remove failed debug configurtion

--- src/about.h
+++ src/about.h
@@ -4,7 +4,7 @@
 #include "versionInfo.cpp"
 
 const std::string app_name = "random_left_game";
-const VersionInfo app_version = VersionInfo(0, 1, 2, "a.zero");
+const VersionInfo app_version = VersionInfo(0, 1, 2, "aazero");
 const std::string app_author = "allan_walpy";
 const std::string app_footer = app_name + " v" + app_version.toString() + " created by " + app_author + ";";
 
