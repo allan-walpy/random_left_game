06972d5c5c0889f3e7418550f30f9b77b2c71f2b
allan_walpy <allanwalpy@gmail.com>
2022-05-12 16:29:20 +0000

    smth

--- add_bgfx.sh
+++ add_bgfx.sh
@@ -4,3 +4,8 @@ wget https://raw.githubusercontent.com/bkaradzic/bgfx/master/bindings/cs/bgfx.cs
 wget https://raw.githubusercontent.com/bkaradzic/bgfx/master/bindings/cs/bgfx_dllname.cs &&
 mkdir ./src/bgfx &&
 mv bgfx* ./src/bgfx
+
+
+echo "see also https://bkaradzic.github.io/bgfx/build.html to build bgfx library"
+echo "use `third` folder for git repositories and all binaries for bgfx"
+mkdir third
